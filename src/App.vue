225b898de26645398ce737c23aa223a0bbf6123a
JBayona <jordavids_22@hotmail.com>
2017-07-22 19:49:31 -0500

    Hot Module replacement and hot reload

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="ocr-container">
+		<div class="message">Value is: {{ count }}</div>
+		<a href="#" v-on:click="increment">Increment</a>
+	</div>
+</template>
+
+<script>
+	export default {
+	  data () {
+	    return {
+	      count: 0
+	    }
+	  },
+	  methods: {
+	    increment () {
+	      this.count ++;
+	    }
+	  }
+	}
+</script>
+
+<style>
+</style>
